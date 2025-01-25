f7c412a64335bc29fe3175fa49d1298e4655b1d6
Samarth Mehrotra <49608778+sam28mehrotra@users.noreply.github.com>
2025-01-25 20:34:54 +0530

    Update FlipkartTest.java

--- FlipkartTest.java
+++ FlipkartTest.java
@@ -3,28 +3,35 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class FlipkartTest {
 
     WebDriver driver;
 
-    @BeforeClass
-    public void setUp(){
-        WebDriverManager.chromedriver().setup();
-        driver =new ChromeDriver();
-    }
-
     @BeforeMethod
-    public  void openbrowser() throws InterruptedException {
+    @Parameters("browser")
+    public void setUp(String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver= new EdgeDriver();
 
+        }
         driver.get("https://www.flipkart.com/");
         driver.manage().window().maximize();
-        Thread.sleep(1000);
     }
+
     @Test
     public void Navigate() throws  InterruptedException{
         WebElement object=driver.findElement(By.xpath("//a[@class='_1ch8e_']"));
